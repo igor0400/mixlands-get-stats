1bc8fe3f5a569f2f05d1dc5bb6ab6d45c085badd
Igor <prostopochtaivso@gmail.com>
2022-10-10 16:49:18 +0300

    переписал на три сервера

--- index.js
+++ index.js
@@ -1,98 +1,90 @@
-const axios = require('axios');
+import axios from 'axios';
+import dotenv from 'dotenv';
 
-const { initializeApp } = require('firebase/app');
-const { getDatabase, ref, set } = require('firebase/database');
+dotenv.config();
 
-////////////////////// НАСТРОЙКА /////////////////////////
+////////////////////// НАСТРОЙКИ /////////////////////////
 
-const serverIP = 'play.mixlands.space'; // IP СЕРВЕРА ДЛЯ СБОРА ДАННЫХ
-const statsDays = 10000; // ПЕРВАЯ ЦИФРА ЭТО КОЛ-ВО ДНЕЙ
-const statsMinutes = 10; // КАЖДЫЕ 10 МИН СБОР ДАННЫХ
-
-const firebaseConfig = {
-   apiKey: 'AIzaSyAvnlJ_k-MkPx0rflE_KZevQ9ddhXubGIk',
-   authDomain: 'mixlands-8fd5a.firebaseapp.com',
-   databaseURL: 'https://mixlands-8fd5a-default-rtdb.firebaseio.com',
-   projectId: 'mixlands-8fd5a',
-   storageBucket: 'mixlands-8fd5a.appspot.com',
-   messagingSenderId: '273559638708',
-   appId: '1:273559638708:web:e931b9b77f3b98bd40fde1',
-   measurementId: 'G-EQKD2FB2BQ',
-};
+const servers = [
+   { name: 'roleplay', port: '25564' },
+   { name: 'creative', port: '25565' },
+   { name: 'adventure', port: '25567' },
+];
 
 /////////////////////////////////////////////////////////
 
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-
-function plusZero(value) {
-   if (value < 10) {
-      value = '0' + value;
-   }
-   return value;
-}
-
 function getDateTime() {
-   const now = new Date();
-   const day = plusZero(now.getDate());
-   const month = plusZero(now.getMonth() + 1);
-   const hours = plusZero(now.getHours());
-   const minutes = plusZero(now.getMinutes());
+   const now = new Date().toLocaleString('ru', {
+      minute: 'numeric',
+      hour: 'numeric',
+      day: 'numeric',
+      month: 'numeric',
+   });
 
-   return `${day}.${month} ${hours}:${minutes}`;
-}
-function getDateTimeId() {
-   const now = new Date();
-   const year = now.getFullYear();
-   const day = plusZero(now.getDate());
-   const month = plusZero(now.getMonth() + 1);
-   const hours = plusZero(now.getHours());
-   const minutes = plusZero(now.getMinutes());
-
-   return `${year}${month}${day}${hours}${minutes}`;
+   return now.replace(',', '');
 }
 
-const postStats = async () => {
+const postServerStats = async ({ name, port }) => {
    let onlinePlayers = 0;
 
    await axios
-      .get(`https://api.mcsrvstat.us/2/${serverIP}`)
-      .then((res) => (res.data.players && res.data.players.online ? onlinePlayers = res.data.players.online : null));
+      .get(`https://api.mcsrvstat.us/2/${process.env.SERVER_IP}:${port}`)
+      .then((res) => {
+         if (res.data?.players?.online) {
+            onlinePlayers = res.data.players.online;
+         }
+      });
 
    const data = {
-      onlinePlayers: onlinePlayers,
+      onlinePlayers,
       date: getDateTime(),
    };
 
-   await set(ref(database, 'online/' + getDateTimeId()), data).catch((err) =>
-      console.log(`Error: ${err}`)
-   );
+   axios
+      .put(`${process.env.DB_LINK}/online/${name}/${Date.now()}.json`, data)
+      .catch((err) => console.log(`Post ${name} sever info error: ${err}`));
 };
 
-const deleteStats = () => {
+const deleteStats = ({ name }) => {
    axios
-      .get(`${firebaseConfig.databaseURL}/online.json`)
+      .get(`${process.env.DB_LINK}/online/${name}.json`)
       .then((res) => {
          const data = res.data;
          const dataKeys = data ? Object.keys(data) : null;
-         const sortDataKeys = dataKeys ? dataKeys.sort((a, b) => a - b) : null;
+         const sortDataKeys = dataKeys
+            ? dataKeys.sort((a, b) => +a - +b)
+            : null;
 
-         const differenceDates = getDateTimeId() - sortDataKeys[0];
-         if (sortDataKeys && differenceDates > statsDays) {
-            set(ref(database, 'online/' + sortDataKeys[0]), null).catch((err) =>
-               console.log(`Error: ${err}`)
-            );
+         if (sortDataKeys) {
+            const differenceDates = Date.now() - +sortDataKeys[0];
+            if (
+               differenceDates >
+               process.env.STATS_DAYS * 24 * 60 * 60 * 1000
+            ) {
+               axios
+                  .delete(
+                     `${process.env.DB_LINK}/online/${name}/${sortDataKeys[0]}.json`
+                  )
+                  .catch((err) =>
+                     console.log(`Delete ${name} server info error: ${err}`)
+                  );
+            }
          }
       })
-      .catch((err) => console.log(`Error: ${err}`));
+      .catch((err) => console.log(`Get server ${name} info error: ${err}`));
 };
 
-postStats();
-deleteStats();
+function start() {
+   servers.forEach((server) => {
+      postServerStats(server);
+      deleteStats(server);
+   });
+}
+
+start();
 
 setInterval(() => {
-   postStats();
-   deleteStats();
-}, 60000 * statsMinutes);
+   start();
+}, 60000 * process.env.STATS_MINUTES);
 
 console.log('Запущено!!!');
